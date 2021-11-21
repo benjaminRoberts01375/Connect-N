c096e1601428d14cf716f1452a54845c46a23b1d
Preposterous <61424934+benjaminRoberts01375@users.noreply.github.com>
2021-11-20 23:45:56 -0500

    Init. lightstrip

--- Connect-N-Project/Connect-N-Project.ino
+++ Connect-N-Project/Connect-N-Project.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
 
+#define LED_PIN 6 //Pin the light strip's dataline is connected to
+#define LED_COUNT 30 //Number of LEDs on the lightstrip (RGB = 1)
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800); //Configure strip
+
+void setup() {
+  strip.begin(); //Initialize NeoPixel strip object
+  strip.show(); //Turn off all LEDs
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
